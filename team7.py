0f2decbbae851f64bc01b3df28151229e906be2b
Hasan Gandor <32725385+HexCodeZero@users.noreply.github.com>
2019-02-28 10:10:14 -0500

    Update team7.py

--- team7.py
+++ team7.py
@@ -11,11 +11,6 @@ strategy_name = 'copycat'
 strategy_description = 'if i cant beat them join them'
     
 def move(my_history, their_history, my_score, their_score):
-    '''
-    Begin with cooperate, then copy opponent's last move.
-    '''
-    
-    #This example player always betrays.      
     if(len(their_history) == 0):
       return 'c'
     else:
